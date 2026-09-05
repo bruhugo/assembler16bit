a502c24f1af9c724909b8388ddd27e38de46983c
Bruno Hugo <brunohugo2007@gmail.com>
2026-09-05 14:31:21 -0300

    better cli

--- main.go
+++ main.go
@@ -1,18 +1,31 @@
 package main
 
 import (
+	"flag"
 	"io"
 	"os"
 
 	assembler "assembler/internal"
 )
 
-func main() {
-	// TODO: make commands better
-	inputFilename := os.Args[1]
-	outputFilename := os.Args[2]
+var (
+	inputFilename  = flag.String("i", "", "input file")
+	outputFilename = flag.String("o", "", "output file")
+	verbose        = flag.Bool("v", false, "verbose output")
+)
 
-	inputFile, err := os.OpenFile(inputFilename, os.O_RDONLY, 0o600)
+func main() {
+	flag.Parse()
+
+	if inputFilename == nil || *inputFilename == "" {
+		panic("Input file must be provided.")
+	}
+
+	if outputFilename == nil || *outputFilename == "" {
+		panic("Output file must be provided.")
+	}
+
+	inputFile, err := os.OpenFile(*inputFilename, os.O_RDONLY, 0o600)
 	if err != nil {
 		panic(err)
 	}
@@ -22,11 +35,11 @@ func main() {
 		panic(err)
 	}
 
-	outputFile, err := os.OpenFile(outputFilename, os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 0o600)
+	outputFile, err := os.OpenFile(*outputFilename, os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 0o600)
 	if err != nil {
 		panic(err)
 	}
 
 	as := assembler.NewAssembler(string(program))
-	as.Assemble(outputFile, assembler.AssembleOptionLogisim, true)
+	as.Assemble(outputFile, assembler.AssembleOptionLogisim, *verbose)
 }
